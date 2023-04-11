6adf7a38ae56fe0c91af27215db6126937bfd220
Can Vy <canvy4btc@gmail.com>
2023-04-11 21:09:07 +0700

    Remove unneccessary file

--- truffle/test/test_main.js
+++
@@ -1,8 +0,0 @@
-const Main = artifacts.require("Main");
-
-contract("Main", function (accounts) {
-  it("should assert true", async function () {
-    await Main.deployed();
-    return assert.isTrue(true);
-  });
-});
